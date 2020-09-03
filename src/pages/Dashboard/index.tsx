3f2168d544ee985c673ffbc2848d648891f049e8
guvarallo <gustavo.varallo@live.com>
2020-09-03 16:56:38 -0300

    connected with GitHub API

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,82 +1,68 @@
-import React from 'react';
+import React, { useState, FormEvent } from 'react';
 import { FiChevronRight } from 'react-icons/fi';
+import api from '../../services/api';
 
 import logo from '../../assets/logo_transparent.png';
 
 import { Title, Form, Repositories } from './styles';
+import Repository from '../Repository';
+
+interface Repository {
+  full_name: string;
+  description: string;
+  html_url: string;
+  owner: {
+    login: string;
+    avatar_url: string;
+  };
+}
 
 const Dashboard: React.FC = () => {
+  const [newRepo, setNewRepo] = useState('');
+  const [repositories, setRepositories] = useState<Repository[]>([]);
+
+  async function handleAddRepository(e: FormEvent): Promise<void> {
+    e.preventDefault();
+
+    const response = await api.get<Repository>(`repos/${newRepo}`);
+
+    const repository = response.data;
+
+    setRepositories([...repositories, repository]);
+    setNewRepo('');
+  }
+
   return (
     <>
       <img id="logo" src={logo} alt="GitHub Explorer" />
       <Title>Explore GitHub Repos</Title>
 
-      <Form>
-        <input placeholder="Type the repository name" />
+      <Form onSubmit={handleAddRepository}>
+        <input
+          value={newRepo}
+          onChange={(e) => setNewRepo(e.target.value)}
+          placeholder="Type the repository name"
+        />
         <button type="submit">Search</button>
       </Form>
 
       <Repositories>
-        <a href="test">
-          <img
-            src="https://avatars2.githubusercontent.com/u/4613916?s=460&u=c012cee5967a79575b2ae7c834496a1a289112ca&v=4"
-            alt="Gus Varallo"
-          />
-          <div>
-            <strong>guvarallo/facerecognition</strong>
-            <p>
-              Face Recon is an app that recongnizes faces in any picture URL
-              using Clarifai&#39;s API.
-            </p>
-          </div>
+        {repositories.map((repo) => (
+          <a
+            key={repo.full_name}
+            href={repo.html_url}
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <img src={repo.owner.avatar_url} alt={repo.owner.login} />
+            <div>
+              <strong>{repo.full_name}</strong>
+              <p>{repo.description}</p>
+            </div>
 
-          <FiChevronRight size={20} />
-        </a>
-        <a href="test">
-          <img
-            src="https://avatars2.githubusercontent.com/u/4613916?s=460&u=c012cee5967a79575b2ae7c834496a1a289112ca&v=4"
-            alt="Gus Varallo"
-          />
-          <div>
-            <strong>guvarallo/facerecognition</strong>
-            <p>
-              Face Recon is an app that recongnizes faces in any picture URL
-              using Clarifai&#39;s API.
-            </p>
-          </div>
-
-          <FiChevronRight size={20} />
-        </a>
-        <a href="test">
-          <img
-            src="https://avatars2.githubusercontent.com/u/4613916?s=460&u=c012cee5967a79575b2ae7c834496a1a289112ca&v=4"
-            alt="Gus Varallo"
-          />
-          <div>
-            <strong>guvarallo/facerecognition</strong>
-            <p>
-              Face Recon is an app that recongnizes faces in any picture URL
-              using Clarifai&#39;s API.
-            </p>
-          </div>
-
-          <FiChevronRight size={20} />
-        </a>
-        <a href="test">
-          <img
-            src="https://avatars2.githubusercontent.com/u/4613916?s=460&u=c012cee5967a79575b2ae7c834496a1a289112ca&v=4"
-            alt="Gus Varallo"
-          />
-          <div>
-            <strong>guvarallo/facerecognition</strong>
-            <p>
-              Face Recon is an app that recongnizes faces in any picture URL
-              using Clarifai&#39;s API.
-            </p>
-          </div>
-
-          <FiChevronRight size={20} />
-        </a>
+            <FiChevronRight size={20} />
+          </a>
+        ))}
       </Repositories>
     </>
   );
